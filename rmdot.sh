c73817534d8bd2b7417510a6ed35d98805d93f7f
berndfinger <49749572+berndfinger@users.noreply.github.com>
2019-09-07 22:17:56 +0200

    Update rmdot.sh

--- rmdot.sh
+++ rmdot.sh
@@ -3,11 +3,11 @@
 NUM_LINES=$(/sbin/mount | awk '/msdos/{$1="";$2="";gsub (" \\(", "\\"); print}' | wc -l)
 
 if [[ ${NUM_LINES} -lt 1 ]]; then
-   echo "Keine Karte gefunden. Exit".
+   echo "No removable media found. Exit".
    exit 1
 fi
 
-echo "${NUM_LINES} Karte(n) gefunden."
+echo "Found ${NUM_LINES} removable media."
 echo
 
 NUM=${NUM_LINES}
@@ -15,29 +15,29 @@ while [[ ${NUM} -gt 0 ]]; do
    DIR=$(/sbin/mount | awk '/msdos/{$1="";$2="";gsub (" \\(", "\\"); print}' | awk 'BEGIN{NUM='"${NUM}"';FS="\\"}NR==NUM{gsub ("^  ", "");printf ("%s", $1)}')
    VOL=$(echo ${DIR} | awk '{gsub ("/Volumes/", ""); print}')
 
-   echo "Aktuelle Karte: \"${VOL}\""
+   echo "Current medium: \"${VOL}\""
    cd "${DIR}"
-   echo "Aktuelles Verzeichnis: \"${DIR}\""
+   echo "Current directory: \"${DIR}\""
    echo
 
    (
-   echo Ueberfluessige Verzeichnisse:
+   echo "Superfluous directories:"
    find . -fstype msdos -type d \( -name .Trashes -o -name .Spotlight-V100 -o -name .fseventsd \)
 
    echo
-   echo Ueberfluessige Dateien:
+   echo "Superfluous files:"
    find . -fstype msdos -type f -name ".??*"
    ) | more
 
    echo
-   echo "Bitte RETURN druecken, um ueberfluessige Verzeichnisse und Dateien zu loeschen:"
+   echo "Press RETURN to remove superfluous directories and files:"
    read a
    find . -fstype msdos -type d \( -name .Trashes -o -name .Spotlight-V100 -o -name .fseventsd \) -exec rm -rf {} \; -print
    find . -fstype msdos -type f -name ".??*" -delete -print
 
    cd
    diskutil eject "${VOL}"
-   echo "Bitte jetzt Speicherkarte \"${VOL}\" entnehmen."
+   echo "Please now remove \"${VOL}\" ."
    echo
    NUM=$((${NUM} - 1))
 done
